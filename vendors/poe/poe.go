b6db73b8c74b96980dde291481ed011eb0cee935
Ruihang Du <11513232+ruihangdu@users.noreply.github.com>
2024-09-17 04:23:40 +0000

    Implement ListModels in poe client

--- vendors/poe/poe.go
+++ vendors/poe/poe.go
@@ -2,18 +2,69 @@ package poe
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
-    "io/ioutil"
-    "net/http"
+	"image/jpeg"
+	"io/ioutil"
+	"net/http"
 	"strings"
+	"time"
 
 	"github.com/danielmiessler/fabric/common"
 )
 
+// TODO: later refactor all the below utils to a different package
+
+type PoeClient struct {
+	BaseURL 	string
+	HTTPClient 	*http.Client
+}
+
+func NewPoeClient() *PoeClient {
+	return &PoeClient{
+		BaseURL: "http://127.0.0.1:8000",
+		HTTPClient: &http.Client{},
+	}
+}
+
+func (p *PoeClient) ListModels(ctx context.Context) ([]string, error) {
+	url := fmt.Sprintf("%s/ListModels", p.BaseURL)
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create ListModels request: %w", err)
+	}
+
+	resp, err := p.HTTPClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to send ListModels request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Received non-OK ListModels response: %w", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read ListModels response body: %w", err)
+	}
+
+	var models []string
+	err = json.Unmarshal(body, &models)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to parse ListModels response: %w", err)
+	}
+
+	return models, nil
+}
+// END TODO
+
 type Client struct {
 	*common.Configurable
 	ApiKey *common.SetupQuestion
+	ApiClient *PoeClient
 }
 
 func NewClient() (ret *Client) {
@@ -26,12 +77,17 @@ func NewClient() (ret *Client) {
 	}
 
 	ret.ApiKey = ret.Configurable.AddSetupQuestion("API key", true)
+	ret.ApiClient = NewPoeClient()
 
 	return
 }
 
 func (o *Client) ListModels() (ret []string, err error) {
 	// Currently a hardcoded list of models because Poe doesn't have API to get all the "bots"
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	ret, err = o.ApiClient.ListModels(ctx)
 	return
 }
 
